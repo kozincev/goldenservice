3da4ec23c6dc59a71c1d6d3a521c9b1331e986f7
finik7123 <kapitanov.2006@mail.ru>
2025-04-25 23:48:22 +0200

    baba

--- vite-project/frontend/src/ttt/ttt.jsx
+++ vite-project/frontend/src/ttt/ttt.jsx
@@ -1,7 +1,14 @@
 export default function ttt() {
     return (
         <>
-
+            <section>
+                <div>
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, tempore alias eveniet laudantium quaerat temporibus quam veniam sit nostrum.</h1>
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, tempore alias eveniet laudantium quaerat temporibus quam veniam sit nostrum.</h1>
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, tempore alias eveniet laudantium quaerat temporibus quam veniam sit nostrum.</h1>
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, tempore alias eveniet laudantium quaerat temporibus quam veniam sit nostrum.</h1>
+                </div>
+            </section>
         </>
     )
 }
